f25b2c66f7bdcb6b3750668c01d9a58c1407c116
Gerardo O <goaccess@prosoftcorp.com>
2022-06-16 19:52:38 -0500

    Added additional debugging code on close tcp.

--- src/websocket.c
+++ src/websocket.c
@@ -2158,6 +2158,8 @@ read_client_data (WSClient * client, WSServer * server) {
 /* Handle a tcp close connection. */
 static void
 handle_tcp_close (int conn, WSClient * client, WSServer * server) {
+    LOG (("Closing TCP %d [%s]\n", client->listener, client->remote_ip));
+
 #ifdef HAVE_LIBSSL
   if (client->ssl)
     shutdown_ssl (client);
